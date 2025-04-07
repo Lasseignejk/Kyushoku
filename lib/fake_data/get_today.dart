1cf6d9c3f8d3c9f5d8a35de4ba1e6465fff78428
lasseignejk <jaye914@hotmail.com>
2025-04-07 17:01:29 -0400

    separated fake data into its own file

---
+++ lib/fake_data/get_today.dart
@@ -0,0 +1,38 @@
+final Map<String, dynamic> getTodayData = {
+  'dishes': [
+    {
+      'name': {
+        'english': 'Chicken Curry',
+        'japanese': 'チキンカレー',
+      },
+      'rating': 5.0,
+      'date_added': '03-14-2025',
+      'photo': '',
+      'is_favorite': true,
+      'tags': ['tasty', 'lunch', 'main dish']
+    },
+    {
+      'name': {
+        'english': 'Shishamo',
+        'japanese': 'シシャモ',
+      },
+      'rating': 0.5,
+      'date_added': '03-29-2025',
+      'photo': '',
+      'is_favorite': false,
+      'tags': ['never again', 'lunch', 'main dish']
+    },
+    {
+      'name': {
+        'english': 'Yakitori',
+        'japanese': null,
+      },
+      'rating': 0.5,
+      'date_added': '03-29-2025',
+      'photo': '',
+      'is_favorite': false,
+      'tags': null,
+    },
+  ],
+  'diary': 'Today I ate lunch with my favorite class.',
+};
