405b509d548ba5f72a502fade370a2478be0f517
lasseignejk <jaye914@hotmail.com>
2025-04-07 16:27:10 -0400

    Created Settings page boilerplate

---
+++ lib/views/settings/settings_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Settings",
+          style: GoogleFonts.frederickaTheGreat(
+              // fontSize: 20,
+              ),
+        ),
+      ),
+      body: Placeholder(),
+    );
+  }
+}
